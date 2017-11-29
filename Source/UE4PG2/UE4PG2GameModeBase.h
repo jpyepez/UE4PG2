67efc0e426c68409c9b60e44f112a005ce723e2c
JP ECS <jpyepez@tokeyo.AD.ECS.VUW.AC.NZ>
2017-11-29 15:35:50 +1300

    started project

---
+++ Source/UE4PG2/UE4PG2GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4PG2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4PG2_API AUE4PG2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
